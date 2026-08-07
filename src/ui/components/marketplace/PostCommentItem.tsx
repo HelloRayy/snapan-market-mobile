1484bd19bd6f0173c06ece0f614ff06e462b5932
Rayhan <rayhanyogiswara133@gmail.com>
2026-08-08 06:49:06 +0700

    style(ui): attach start of L-curved line directly to bottom of Person A avatar without gap

--- src/ui/components/marketplace/PostCommentItem.tsx
+++ src/ui/components/marketplace/PostCommentItem.tsx
@@ -161,7 +161,10 @@ export const PostCommentItem: React.FC<PostCommentItemProps> = ({
         </div>
       ) : (
         /* THREAD COMMENT WITH TOP REPLY: Indented child reply + L-shaped curved connecting line (└─) */
-        <div className="space-y-3">
+        <div className="relative space-y-3">
+          {/* L-Shaped Curved Branch Line: Starts touching bottom of Person A avatar (top-[32px]), curves into left center of Person B avatar */}
+          <div className="absolute left-[17px] top-[32px] bottom-[24px] w-[12px] border-l-2 border-b-2 border-[#e5e7eb] rounded-bl-xl pointer-events-none" />
+
           {/* Parent Comment Row */}
           <div className="flex items-start gap-3">
             {/* Left Parent Avatar (36x36px) */}
@@ -218,11 +221,8 @@ export const PostCommentItem: React.FC<PostCommentItemProps> = ({
             </div>
           </div>
 
-          {/* Child Reply Row (Indented right with ml-7, L-curve ends at left center of child avatar!) */}
-          <div className="flex items-start gap-3 ml-7 relative">
-            {/* L-Shaped Curved Branch Line: Starts from parent avatar bottom, curves into left center of child avatar (18px down) */}
-            <div className="absolute -left-[11px] -top-3.5 h-[36px] w-[11px] border-l-2 border-b-2 border-[#e5e7eb] rounded-bl-xl pointer-events-none" />
-
+          {/* Child Reply Row (Indented right with ml-7) */}
+          <div className="flex items-start gap-3 ml-7">
             {/* Left Child Avatar (36x36px) */}
             <div className="w-9 h-9 rounded-full overflow-hidden border border-neutral-200/80 shadow-2xs shrink-0 z-10 bg-white">
               <img
